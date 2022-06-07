ca5531971cba16e640ca2099cb11972d82abb3fc
Andie <20520377@gm.uit.edu.vn>
2022-06-07 14:46:56 +0700

    sec

---
+++ scripts/file1.c
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a=2, b=4;
+	int res = a + b;
+	cout << res;
+	return 0;
+}
---
+++ scripts/file2.c
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a=0, b=0;
+	int res = a - b + 10;
+	cout << res;
+	return 0;
+}
